259ae7c22c4ec2382392a8a4d725aac18623de45
David <david48kariuki@gmail.com>
2025-05-12 11:09:16 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.base_word = word.lower()
+
+    def match(self, candidates):
+        # Filter the list to only include valid anagrams
+        return [
+            candidate for candidate in candidates
+            if sorted(candidate.lower()) == sorted(self.base_word) and candidate.lower() != self.base_word
+        ]
